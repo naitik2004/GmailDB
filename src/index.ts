4bcb56fbde21bfdc8e121c5565a06bc86e41e056
naitik2004 <naitik123456789pandey@gmail.com>
2026-05-14 08:39:31 +0530

    build: clean compile, npm publish prep

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { GmailDB } from './sdk/index.js';
+export { Collection } from './sdk/collection.js';
+export { GmailEngine, createEngine } from './core/engine.js';
+export { authenticate, createOAuthClient, loadSavedToken } from './auth/oauth.js';
+export { encrypt, decrypt } from './core/encryption.js';
